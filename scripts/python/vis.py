015d73d0ee5c3b1646643ff61a11110d7a713b5d
hypnotic2402 <saket.gurjar@iiitb.ac.in>
2024-03-28 19:44:48 +0530

    Presentations added

--- scripts/python/vis.py
+++ scripts/python/vis.py
@@ -3,22 +3,6 @@ import numpy as np
 import time
 
 
-# out = cv2.VideoWriter('output_video.avi',0,1, (800,800))
-# img1 = np.zeros((800, 800, 3), np.uint8) 
-# cv2.rectangle(img1,(100,100) , (200,200) , (0,0,255) , 3)
-
-# img2 = np.zeros((800, 800, 3), np.uint8) 
-# cv2.rectangle(img2,(200,200) , (300,300) , (0,0,255) , 3)
-
-# img3 = np.zeros((800, 800, 3), np.uint8) 
-# cv2.rectangle(img3,(400,400) , (400,400) , (0,0,255) , 3)
-
-# out.write(img1)
-# out.write(img2)
-# out.write(img3)
-
-# cv2.destroyAllWindows()  
-# out.release()
 
 
 def genVid(XHis , wh):
@@ -42,23 +26,4 @@ def genVid(XHis , wh):
     out.release()
     
 
-        
-
-
-# generate_video() 
-# out.write(img)
-
-# img = np.zeros((800, 800, 3), np.uint8) 
-# cv2.rectangle(img,(200,200) , (300,300) , (0,0,255) , 3)
-# out.write(img)
-# cv2.imshow('image', img)
-# time.sleep(3)
-# cv2.imshow('image', img)
- 
-# Maintain output window until
-# user presses a key
-# cv2.waitKey(0)
- 
-# Destroying present windows on screen
-# cv2.destroyAllWindows()
 
