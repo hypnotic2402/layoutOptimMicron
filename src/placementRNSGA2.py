1e0d602b7ae56bffbe4ff98ef1f7a01f48997684
Krutik Patel <103781172+Krutik-Patel@users.noreply.github.com>
2024-08-31 13:46:28 +0530

    Add files via upload
    Former-commit-id: 702cd5302403b4f401891823c534f0ce79dafcd6

--- src/placementRNSGA2.py
+++ src/placementRNSGA2.py
@@ -13,8 +13,8 @@ class Individual:
         self.rank = None
         self.crowding_distance = 0
 
-class NSGAII:
-    def __init__(self, pop_size, crossover_prob, mutation_prob, objF, x_len, xl, xu, nets, wh, margin=0):
+class RNSGAII:
+    def __init__(self, pop_size, crossover_prob, mutation_prob, objF, x_len, xl, xu, nets, wh, ref_point, margin=0):
         self.pop_size = pop_size
         self.crossover_prob = crossover_prob
         self.mutation_prob = mutation_prob
@@ -23,7 +23,8 @@ class NSGAII:
         self.xl = xl
         self.xu = xu
         self.nets = nets
-        self.wh = [x+margin for x in wh]
+        self.wh = [x + margin for x in wh]
+        self.ref_point = ref_point
         self.logger = Logger.getInstance()
         self.population = self.initialize_population()
 
@@ -48,7 +49,6 @@ class NSGAII:
             if random.random() < self.mutation_prob:
                 individual.position[i] = random.uniform(self.xl, self.xu)
         individual.objectives = self.objF(individual.position, self.wh, self.nets)
-        # print(individual.objectives)
 
     def select_parents(self):
         tournament_size = 2
@@ -99,7 +99,6 @@ class NSGAII:
                     for j in range(1, len(front) - 1):
                         front[j].crowding_distance += (front[j + 1].objectives[i] - front[j - 1].objectives[i]) / (max_obj - min_obj)
 
-
     def dominates(self, individual1, individual2):
         better_in_all = all(x <= y for x, y in zip(individual1.objectives, individual2.objectives))
         better_in_at_least_one = any(x < y for x, y in zip(individual1.objectives, individual2.objectives))
@@ -121,31 +120,33 @@ class NSGAII:
         new_population = []
         for front in fronts:
             self.crowding_distance_assignment(front)
-            front.sort(key=lambda x: (x.rank, -x.crowding_distance))
+            front.sort(key=lambda x: self.calculate_r2_indicator(x))
             new_population.extend(front)
             if len(new_population) >= self.pop_size:
                 break
         self.population = new_population[:self.pop_size]
 
+    def calculate_r2_indicator(self, individual):
+        r2_value = max([w * abs(o - rp) for w, o, rp in zip([1]*len(individual.objectives), individual.objectives, self.ref_point)])
+        return r2_value
+
     def opt(self, n_iter):
         print("n_iter: ", n_iter)
         for iter in range(n_iter):
             if iter % 10 == 0:
-                best = min(self.population, key=lambda x: x.objectives[0])
-                # best = self.population[-1]
-                # print(self.population[0])
+                best = min(self.population, key=lambda x: self.calculate_r2_indicator(x))
                 self.logger.log(f"Iteration {iter} ----> Overlap: {best.objectives[0]} HPWL: {best.objectives[1]} Distance: {best.objectives[2]}")
                 print(f"Iteration {iter} ----> Overlap: {best.objectives[0]} HPWL: {best.objectives[1]} Distance: {best.objectives[2]}")
             offspring = self.generate_offspring()
             self.replace_population(offspring)
 
-        self.best = min(self.population, key=lambda x: x.objectives[0])
+        self.best = min(self.population, key=lambda x: self.calculate_r2_indicator(x))
         isOverlapping(self.best.position, self.wh)
         x_min, y_min, x_max, y_max, tot_area = getBoundingBox(self.best.position, self.wh)
 
 class PlacementSolver:
     floor = None
-    def __init__(self, macros, nets, floor, pop_size, margin=0):
+    def __init__(self, macros, nets, floor, pop_size, ref_point, margin=0):
         self.macros = macros
         self.Nets = nets
         self.floor = floor
@@ -158,12 +159,11 @@ class PlacementSolver:
         for macro in macros:
             self.wh.append(macro.w)
             self.wh.append(macro.h)
-        self.nsga2 = NSGAII(pop_size, 0.7, 0.01, objF, 2*len(self.macros), 0, self.floor.h - max(self.wh), self.nets, self.wh, 1)
-
+        self.rnsga2 = RNSGAII(pop_size, 0.7, 0.01, objF, 2*len(self.macros), 0, self.floor.h - max(self.wh), self.nets, self.wh, ref_point, 1)
     def place(self, iter):
-        print("NSGA2 in here...")
-        self.nsga2.opt(iter)
-        X = self.nsga2.best.position
+        print("RNSGA2 in here...")
+        self.rnsga2.opt(iter)
+        X = self.rnsga2.best.position
         for i in range(len(self.macros)):
             self.macros[i].x = min(X[(2*i)], self.floor.w - self.wh[2*i])
             self.macros[i].y = min(X[(2*i) + 1], self.floor.h - self.wh[2*i + 1])
@@ -171,7 +171,7 @@ class PlacementSolver:
     def genVid(self, path, full_video=False):
         if not full_video:
             img1 = np.zeros((self.floor.h, self.floor.w, 3), np.uint8)
-            X = self.nsga2.population[0].position
+            X = self.rnsga2.population[0].position
             for i in range(int(len(X) / 2)):
                 xi_min = self.macros[i].x
                 yi_min = self.macros[i].y
@@ -182,7 +182,7 @@ class PlacementSolver:
             cv2.imwrite(path, img1)
         else:
             out = cv2.VideoWriter(path, 0, 40, (self.floor.w, self.floor.h))
-            for frame in self.nsga2.population:
+            for frame in self.rnsga2.population:
                 img1 = np.zeros((self.floor.w, self.floor.h, 3), np.uint8)
                 X = frame.position
                 for i in range(int(len(X) / 2)):
@@ -190,114 +190,66 @@ class PlacementSolver:
                     yi_min = X[2*i + 1]
                     xi_max = xi_min + self.wh[2*i]
                     yi_max = yi_min + self.wh[2*i + 1]
-                    cv2.rectangle(img1, (int(xi_min), int(yi_min)), (int(xi_max), int(yi_max)), (0, 0, 255), 3)
+                    cv2.rectangle(img1, (int(xi_min), int(yi_min)), (int(xi_max), int(yi_max)), (0,0,255), 3)
+                    cv2.putText(img1, f"{self.macros[i].name}", (int(xi_min), int(yi_min)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,255,255), 2)
                 out.write(img1)
-            cv2.destroyAllWindows()
             out.release()
 
-def hpwl(X, wh, nets):
-    s = 0
-    for net in nets:
-        xmin = math.inf
-        xmax = -math.inf
-        ymin = math.inf
-        ymax = -math.inf
-        for i in range(len(net)):
-            x = X[2*net[i]]
-            y = X[2*net[i] + 1]
-            if x < xmin:
-                xmin = x
-            if x > xmax:
-                xmax = x
-            if y < ymin:
-                ymin = y
-            if y > ymax:
-                ymax = y
-        s += (xmax - xmin) + (ymax - ymin)
-    return s
+def isOverlapping(X, wh):
+    for i in range(int(len(X) / 2)):
+        xi_min = X[(2*i)]
+        yi_min = X[(2*i) + 1]
+        xi_max = xi_min + wh[2*i]
+        yi_max = yi_min + wh[2*i + 1]
+        for j in range(i+1, int(len(X) / 2)):
+            xj_min = X[(2*j)]
+            yj_min = X[(2*j) + 1]
+            xj_max = xj_min + wh[2*j]
+            yj_max = yj_min + wh[2*j + 1]
+            if (xi_max > xj_min and xj_max > xi_min and yi_max > yj_min and yj_max > yi_min):
+                print(f"Overlap between {i+1} and {j+1} with coordinates: {xi_min}, {yi_min}, {xi_max}, {yi_max} and {xj_min}, {yj_min}, {xj_max}, {yj_max}")
 
-def hpwlFaster(X, wh, nets):
-    s = 0
+def objF(X, wh, nets):
+    overlap_area = 0
+    hpwl = 0
+    min_dist = 0
+    for i in range(int(len(X) / 2)):
+        xi_min = X[(2*i)]
+        yi_min = X[(2*i) + 1]
+        xi_max = xi_min + wh[2*i]
+        yi_max = yi_min + wh[2*i + 1]
+        for j in range(i+1, int(len(X) / 2)):
+            xj_min = X[(2*j)]
+            yj_min = X[(2*j) + 1]
+            xj_max = xj_min + wh[2*j]
+            yj_max = yj_min + wh[2*j + 1]
+            dx = min(xi_max, xj_max) - max(xi_min, xj_min)
+            dy = min(yi_max, yj_max) - max(yi_min, yj_min)
+            if (dx >= 0 and dy >= 0):
+                overlap_area += dx * dy
+
+    Xx, Xy = [], []
+    for i in range(int(len(X) / 2)):
+        Xx.append(X[(2*i)] + wh[2*i] / 2)
+        Xy.append(X[(2*i) + 1] + wh[2*i + 1] / 2)
+
     for net in nets:
-        xmin = min(X[2*net[i]] for i in range(len(net)))
-        xmax = max(X[2*net[i]] for i in range(len(net)))
-        ymin = min(X[2*net[i] + 1] for i in range(len(net)))
-        ymax = max(X[2*net[i] + 1] for i in range(len(net)))
-        s += (xmax - xmin) + (ymax - ymin)
-    return s
+        x = [Xx[i] for i in net]
+        y = [Xy[i] for i in net]
+        hpwl += (max(x) - min(x) + max(y) - min(y))
+
+    min_dist = getBoundingBox(X, wh)[4]
+    return (overlap_area, hpwl, min_dist)
 
 def getBoundingBox(X, wh):
-    x_min = float('inf')
-    x_max = float('-inf')
-    y_min = float('inf')
-    y_max = float('-inf')
-    total_area = 0
+    min_x, min_y, max_x, max_y = float('inf'), float('inf'), -float('inf'), -float('inf')
+    area = 0
     for i in range(int(len(X) / 2)):
-        xi_min = X[2*i]
-        yi_min = X[2*i + 1]
-        xi_max = xi_min + wh[2*i]
-        yi_max = yi_min + wh[2*i + 1]
-        x_min = min(x_min, xi_min)
-        x_max = max(x_max, xi_max)
-        y_min = min(y_min, yi_min)
-        y_max = max(y_max, yi_max)
-        total_area += (yi_max - yi_min) * (xi_max - xi_min)
-    return x_min, y_min, x_max, y_max, total_area
+        min_x = min(min_x, X[(2*i)])
+        min_y = min(min_y, X[(2*i) + 1])
+        max_x = max(max_x, X[(2*i)] + wh[2*i])
+        max_y = max(max_y, X[(2*i) + 1] + wh[2*i + 1])
+        area += wh[2*i] * wh[2*i + 1]
+    return min_x, min_y, max_x, max_y, ((max_x - min_x) * (max_y - min_y)) - area
 
-def distance_from_edges(X, wh, floor_w, floor_h):
-    distance = 0
-    for i in range(int(len(X) / 2)):
-        xi_min = X[2*i]
-        yi_min = X[2*i + 1]
-        xi_max = xi_min + wh[2*i]
-        yi_max = yi_min + wh[2*i + 1]
-        distance += xi_min + yi_min + (floor_w - xi_max) + (floor_h - yi_max)
-    return distance
 
-def isOverlapping(X, wh):
-    total_overlapp = 0
-    for i in range(int(len(X) / 2)):
-        xi_min = X[2*i]
-        yi_min = X[2*i + 1]
-        xi_max = xi_min + wh[2*i]
-        yi_max = yi_min + wh[2*i + 1]
-        for j in range(int(len(X) / 2)):
-            if i != j:
-                xj_min = X[2*j]
-                yj_min = X[2*j + 1]
-                xj_max = xj_min + wh[2*j]
-                yj_max = yj_min + wh[2*j + 1]
-                dx = min(xi_max , xj_max) - max(xi_min , xj_min)
-                dy = min(yi_max , yj_max) - max(yi_min , yj_min)
-                if (dx>=0) and (dy>=0):
-                    total_overlapp += dx*dy
-    return total_overlapp
-
-def isOverlappingFaster(X, wh):
-    # print("X: ", X)
-    # print("wh: ", wh)
-    # quit the program
-
-    X = np.array(X).reshape(-1, 2)
-    wh = np.array(wh).reshape(-1, 2)
-    X_max = X + wh
-
-    dx = np.maximum(0, np.minimum(X_max[:, None, 0], X_max[None, :, 0]) - np.maximum(X[:, None, 0], X[None, :, 0]))
-    dy = np.maximum(0, np.minimum(X_max[:, None, 1], X_max[None, :, 1]) - np.maximum(X[:, None, 1], X[None, :, 1]))
-
-    overlap_areas = dx * dy
-
-    np.fill_diagonal(overlap_areas, 0)  # Exclude self-overlap
-
-    total_overlap = np.sum(overlap_areas)
-    return total_overlap
-
-def objF(X , wh , nets):
-    overlap = isOverlappingFaster(X, wh)
-    _, _, _, _, total_area = getBoundingBox(X, wh)
-    floor_w, floor_h = PlacementSolver.floor.w, PlacementSolver.floor.h
-    distance = distance_from_edges(X, wh, floor_w, floor_h)
-    hpwl = hpwlFaster(X, wh, nets)
-    return overlap, hpwl, distance
-
-# 
